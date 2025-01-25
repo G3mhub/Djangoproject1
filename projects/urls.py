e779db64ae97bec057646a6af88c3483b4603d13
GemHABD <alejandroomar487@gmail.com>
2025-01-25 10:14:06 -0400

    add navbar

---
+++ projects/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from projects.views import ListProjects,DetailProjects
+
+urlpatterns=[
+
+    path("",ListProjects.as_view()),
+    path("detail/<int:pk>",DetailProjects.as_view(),name="detail")
+]
---
+++ projects/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from projects.models import Post
+
+# Create your views here.
+
+class ListProjects(ListView):
+    model=Post
+    template_name="home.html"
+
+class DetailProjects(DetailView):
+    model=Post
+    template_name="detail.html"
+
