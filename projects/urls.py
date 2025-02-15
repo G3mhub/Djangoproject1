9861a4d8d33148380908e16308053e70697fb751
GemHABD <alejandroomar487@gmail.com>
2025-02-15 11:10:17 -0400

    add welcome html

--- projects/urls.py
+++ projects/urls.py
@@ -4,7 +4,8 @@ from projects.views import *
 
 urlpatterns=[
 
-    path("",ListProjects.as_view(),name="home"),
+    path("",WelcomePage,name="welcome"),
+    path("list",ListProjects.as_view(),name="home"),
     path("detail/<int:pk>",DetailProjects.as_view(),name="detail"),
     path("Create",CreatePageView.as_view(), name= "create"),
     path("detail/<int:pk>/update",UpdatePageView.as_view(),name="update"),
--- projects/views.py
+++ projects/views.py
@@ -31,3 +31,7 @@ class DeletePageView(DeleteView):
     model=Post
     template_name="delete.html"
     success_url = reverse_lazy("home")
+
+#vista para la pagina de inicio
+def WelcomePage(request):
+    return render(request,"welcome.html")
