d034f2fecee8a34c91f174faa9dc5da41a575882
GemHABD <alejandroomar487@gmail.com>
2025-02-22 09:49:21 -0400

    crudcss styles

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from authentication.views import *
 
 urlpatterns=[
-    path("", SingUpView.as_view(),name="singup"),
+    path("", SingUpView.as_view(),name="signup"),
     path("accounts/",include("django.contrib.auth.urls")),
 
 ]
